9569751639e548080ffe0c01243df3d5b29b5930
Dominic Létourneau <doumdi@gmail.com>
2024-01-23 11:27:31 -0500

    First version of UserComManager, need to inherit from base class

---
+++ cpp/clients/user/src/UserComManager.h
@@ -0,0 +1,112 @@
+#ifndef UserComManager_H
+#define UserComManager_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkCookieJar>
+#include <QNetworkReply>
+#include <QUrlQuery>
+#include <QAuthenticator>
+#include <QHttpMultiPart>
+#include <QHttpPart>
+#include <QTimer>
+
+#include "UserWebSocketManager.h"
+
+
+
+class UserComManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UserComManager(QObject *parent = nullptr);
+
+public slots:
+
+    void loginToServer(QString username, QString password, QString server_name);
+    void login();
+    void getOnlineParticipants();
+    void getOnlineDevices();
+    void getSessionTypes();
+
+
+    void setUsername(const QString& username) {
+        m_username = username;
+    }
+
+    void setPassword(const QString& password) {
+        m_password = password;
+    }
+
+    void setServerUrl(const QString &url) {
+        m_serverUrl = QUrl(url);
+    }
+
+signals:
+
+    void loginSucceeded();
+    void loginFailed(const QString &errorMessage);
+
+    void participantOnline(const int id_participant, const int id_participant_group, const int id_project,
+                           const QString &participant_email, const QString &participant_name, const bool participant_token_enabled,
+                           const QString &participant_uuid, const bool participant_online, const bool participant_busy);
+
+    void deviceOnline(const bool device_busy, const bool device_enable, const QString &device_name, const bool device_online,
+                      const qint64 timestamp, const QString &status_string, const QString &device_uuid, const int id_device,
+                      const int id_device_subtype, const int id_device_type);
+
+
+    void participantOffine(const QString &name, const QString &uuid);
+    void deviceOffline(const QString &name, const QString &uuid);
+
+    void sessionTypeAvailable(const int id_service, const int id_session_type, const int session_type_category,
+                              const QString &session_type_color, const QString &session_type_name, const bool session_type_online,
+                              const QString &session_type_service_key, const QString &session_type_service_uuid);
+
+
+    void sessionStarted(const int id_session, const int id_session_type, const QString &session_key,
+                        const QString &session_name, const QString &session_uuid, const QString &session_start_datetime, const int session_duration);
+
+    void sessionStartError(const QString &message);
+
+    void sessionStopped(const int id_session, const int id_session_type, const QString &session_key,
+                        const QString &session_name, const QString &session_uuid, const QString &session_start_datetime, const int session_duration);
+
+    void sessionStopError(const QString &message);
+
+    void tokenRefreshed(const QString &token);
+
+private slots:
+
+    void	onNetworkAuthenticationRequired(QNetworkReply *reply, QAuthenticator *authenticator);
+
+    void	onNetworkFinished(QNetworkReply *reply);
+
+#ifndef OPENTERA_WEBASSEMBLY
+    void	onNetworkEncrypted(QNetworkReply *reply);
+    void	onNetworkSslErrors(QNetworkReply *reply, const QList<QSslError> &errors);
+#endif
+    void    onRefreshTokenTimeout();
+
+private:
+    QNetworkAccessManager *m_networkAccessManager;
+    UserWebSocketManager *m_websocketManager;
+    QString m_username;
+    QString m_password;
+    QString m_token;
+    QUrl m_serverUrl;
+    QTimer *m_refreshTokenTimer;
+
+
+    QNetworkReply* _doPost(const QUrl &url, const QUrlQuery &query_args = QUrlQuery(), const QString &data = QString(), bool use_token = true);
+    QNetworkReply* _doGet(const QUrl &url, const QUrlQuery &query_args = QUrlQuery(), bool use_token = true);
+    void _setRequestLanguage(QNetworkRequest &request);
+    void _setRequestCredentials(QNetworkRequest &request, const bool &use_token);
+    void _setRequestVersions(QNetworkRequest &request);
+    void _connectWebSocket(const QUrl &url);
+    void _startRefreshTokenTimer();
+    void _stopRefreshTokenTimer();
+    void _refreshToken();
+};
+
+#endif // UserComManager_H
---
+++ cpp/clients/user/src/UserWebSocketManager.h
@@ -0,0 +1,32 @@
+#ifndef USERWEBSOCKETMANAGER_H
+#define USERWEBSOCKETMANAGER_H
+
+#include <QObject>
+#include <QUrl>
+#include <QWebSocket>
+
+class UserWebSocketManager : public QObject
+{
+    Q_OBJECT
+
+    QWebSocket *m_websocket;
+
+public:
+    explicit UserWebSocketManager(QObject*parent = nullptr);
+
+    void connectTo(const QUrl &url);
+    void close();
+
+signals:
+
+private slots:
+    void onBinaryMessageReceived(const QByteArray &message);
+    void onTextMessageReceived(const QString &message);
+#ifndef OPENTERA_WEBASSEMBLY
+    void onSslErrors(const QList<QSslError> &errors);
+#endif
+    void onConnected();
+    void onDisconnected();
+
+};
+#endif // USERWEBSOCKETMANAGER_H
