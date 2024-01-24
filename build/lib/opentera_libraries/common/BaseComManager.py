067d6b6bc34fdeb752be207b5963d9bf0b263d7b
Simon Briere <simon.briere@usherbrooke.ca>
2024-01-24 15:25:34 -0500

    Changed python structure to build a proper pip wheel.

---
+++ build/lib/opentera_libraries/common/BaseComManager.py
@@ -0,0 +1,92 @@
+from requests import Response, Session
+from abc import ABC, abstractmethod
+import locale
+import logging
+import platform
+import json
+import urllib3
+
+
+class BaseComManager(ABC):
+
+    def __init__(self, server_url: str, server_port: int, allow_insecure=False):
+        self._token = None
+        self.server_url = 'https://' + server_url + ":" + str(server_port)
+        self.language = locale.getlocale()[0]  # Default language = system
+        self.os_name = platform.system() + ' ' + platform.release()
+        self.os_version = platform.version()
+        self.client_name = 'OpenTera Libraries'
+        self.client_version = '1.0.0'   # TODO: Use real version number of the libraries
+        self.verify_ssl = not allow_insecure
+        if not self.verify_ssl:
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        # Init logger
+        self.set_logging_level(level=logging.CRITICAL)
+
+        # Init request session
+        self._session = Session()
+        self._set_session_headers()
+
+    @property
+    def token(self):
+        return self._token
+
+    @token.setter
+    def token(self, value):
+        self._token = value
+        # Update headers in request session
+        self._set_session_headers()
+
+    @staticmethod
+    def set_logging_level(level: int):
+        logging.basicConfig(level=level, format='%(asctime)s\t[%(levelname)s]\t%(module)s\t%(message)s')
+
+    def do_get(self, endpoint: str, params: dict | None = None, extra_headers: dict | None = None) -> Response:
+        logging.debug(msg='GET ' + endpoint + ', ' + str(params))
+        if extra_headers:
+            headers = self._session.headers | extra_headers
+            return self._session.get(url=self.server_url + endpoint, params=params, verify=self.verify_ssl,
+                                     headers=headers)
+        else:
+            return self._session.get(url=self.server_url + endpoint, params=params, verify=self.verify_ssl)
+
+    def do_post(self, endpoint: str, data: str, extra_headers: dict | None = None) -> Response:
+        logging.debug(msg='POST ' + endpoint + ', ' + json.dumps(data))
+        if extra_headers:
+            headers = self._session.headers | extra_headers
+            return self._session.post(url=self.server_url + endpoint, json=data, headers=headers, verify=self.verify_ssl)
+        else:
+            return self._session.post(url=self.server_url + endpoint, json=data, verify=self.verify_ssl)
+
+    def do_delete(self, endpoint: str, id_to_delete: int, extra_headers: dict | None = None) -> Response:
+        logging.debug(msg='DELETE ' + endpoint + ', ' + str(id_to_delete))
+        if extra_headers:
+            headers = self._session.headers | extra_headers
+            return self._session.delete(url=self.server_url + endpoint, params={'id': id_to_delete}, headers=headers,
+                                        verify=self.verify_ssl)
+        else:
+            return self._session.delete(url=self.server_url + endpoint, params={'id': id_to_delete},
+                                        verify=self.verify_ssl)
+
+    def _set_session_headers(self):
+        headers = {
+            'Accept-Language': self.language,
+            'X-OS-Name': self.os_name,
+            'X-OS_Version': self.os_version,
+            'X-Client-Name': self.client_name,
+            'X-Client-Version': self.client_version
+        }
+
+        if self._token:
+            headers['Authorization'] = 'OpenTera ' + self._token
+
+        self._session.headers = headers
+
+    @abstractmethod
+    def login_with_username(self, username: str, password: str) -> Response | None:
+        pass
+
+    @abstractmethod
+    def login_with_token(self, token: str) -> Response | None:
+        pass
---
+++ build/lib/opentera_libraries/device/DeviceComManager.py
@@ -0,0 +1,33 @@
+from requests import Response
+import logging
+import device.DeviceAPI
+
+from common.BaseComManager import BaseComManager
+
+
+class DeviceComManager(BaseComManager):
+
+    def __init__(self, server_url: str, server_port: int, allow_insecure=False):
+        super().__init__(server_url=server_url, server_port=server_port, allow_insecure=allow_insecure)
+
+    def register_device(self, server_key: str, device_name: str, device_type_key: str,
+                        device_subtype_name: str | None = None) -> Response:
+        params = {'name': device_name, 'key': server_key, 'type_key': device_type_key}
+        if device_subtype_name:
+            params['subtype_name'] = device_type_key
+
+        return self.do_get(device.DeviceAPI.ENDPOINT_DEVICE_REGISTER, params)
+
+    def login_with_token(self, token: str) -> Response | None:
+        params = {'token': token}
+        response = self.do_get(device.DeviceAPI.ENDPOINT_DEVICE_LOGIN, params)
+        if response.status_code == 200:
+            self.token = token
+        else:
+            self.token = None
+
+        return response
+
+    def login_with_username(self, username: str, password: str) -> Response | None:
+        logging.warning(msg='Device can\'t log with username - password')
+        return None
