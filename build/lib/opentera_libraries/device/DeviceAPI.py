0ab3622f54b6c4d52d00a231423b97f22c88fa34
Simon Briere <simon.briere@usherbrooke.ca>
2024-01-29 07:57:12 -0500

    Removed and ignored build dir.

--- build/lib/opentera_libraries/device/DeviceAPI.py
+++
@@ -1,3 +0,0 @@
-
-ENDPOINT_DEVICE_REGISTER = '/api/device/register'
-ENDPOINT_DEVICE_LOGIN = '/api/device/login'
--- build/lib/opentera_libraries/user/UserAPI.py
+++
@@ -1,3 +0,0 @@
-ENDPOINT_USER_LOGIN = '/api/user/login'
-ENDPOINT_USER_DEVICES = '/api/user/devices'
-ENDPOINT_USER_SERVER_SETTINGS = '/api/user/server/settings'
